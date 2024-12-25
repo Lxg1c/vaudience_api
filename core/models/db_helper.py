89e5ce27859422d83853abfed969fef8e5b59a01
Lxg1c <teran_2005@mail.ru>
2024-12-25 14:43:36 +0300

    Added method to get current scope session

--- core/models/db_helper.py
+++ core/models/db_helper.py
@@ -1,4 +1,10 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from asyncio import current_task
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    async_scoped_session,
+    AsyncSession,
+)
 from core.config import settings
 
 
@@ -9,6 +15,17 @@ class DataBaseHelper:
         self.session_factory = async_sessionmaker(
             bind=self.engine, autoflush=False, autocommit=False, expire_on_commit=False
         )
+
+    def get_scoped_session(self):
+        session = async_scoped_session(
+            session_factory=self.session_factory, scopefunc=current_task
+        )
+        return session
+
+    async def session_dependency(self) -> AsyncSession:
+        async with self.get_scoped_session() as session:
+            yield session
+            await session.remove()
 
 
 db_helper = DataBaseHelper(url=settings.db_url, echo=settings.db_echo)
