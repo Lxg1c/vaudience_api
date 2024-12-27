540230914a7fdf5ede74b582e2ff3ff580b66091
Lxg1c <teran_2005@mail.ru>
2024-12-27 08:58:46 +0300

    user ready to export

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,5 +1,12 @@
-__all__ = ("Base", "Product", "DataBaseHelper", "db_helper")
+__all__ = (
+    "Base",
+    "Product",
+    "DataBaseHelper",
+    "db_helper",
+    "User",
+)
 
 from .base import Base
 from .product import Product
 from .db_helper import DataBaseHelper, db_helper
+from .user import User
