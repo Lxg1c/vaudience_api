0593b35a7731ebed930be19393c44e3e5beb2fa2
Lxg1c <teran_2005@mail.ru>
2024-12-27 09:07:45 +0300

    Create posts model

---
+++ core/models/post.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from .base import Base
+from sqlalchemy import String, Text, ForeignKey
+
+
+class Post(Base):
+    title: Mapped[str] = mapped_column(
+        String(100),
+        unique=False,
+    )
+    body: Mapped[str] = mapped_column(
+        Text,
+        default="",
+        server_default="",
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+        # nullable=False,
+    )
