323d54406430e1f064bc415631b20d8273bfada6
Lxg1c <teran_2005@mail.ru>
2025-01-10 15:37:35 +0300

    add access and refresh expire

--- core/config.py
+++ core/config.py
@@ -10,6 +10,8 @@ class AutJWT(BaseModel):
     private_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
     public_key_path: Path = BASE_DIR / "certs" / "jwt-public.pem"
     algorithm: str = "RS256"
+    access_token_expire_minutes: int = 15
+    refresh_token_expire_days: int = 30
 
 class DBSettings(BaseModel):
     url: str = f"sqlite+aiosqlite:///{BASE_PATH}"
