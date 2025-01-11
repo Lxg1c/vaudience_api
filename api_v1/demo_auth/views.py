66a26bdd1fd80d7b43e42c2024300e992c8d637c
Lxg1c <teran_2005@mail.ru>
2025-01-11 15:04:09 +0300

    add full auth

--- api_v1/demo_auth/views.py
+++ api_v1/demo_auth/views.py
@@ -1,22 +1,22 @@
 import secrets
 import uuid
+from typing import Annotated, Any
 from time import time
-from typing import Annotated
-from fastapi import APIRouter, Depends, HTTPException, Header, Response, Cookie
-from fastapi.security import HTTPBasicCredentials, HTTPBasic
-from starlette import status
 
-router = APIRouter(prefix="/demo-auth", tags=["Demo-Auth"])
+from fastapi import APIRouter, Depends, HTTPException, status, Header, Response, Cookie
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+
+router = APIRouter(prefix="/demo-auth", tags=["Demo Auth"])
 
 security = HTTPBasic()
 
 
-@router.get("/basic-auth")
+@router.get("/basic-auth/")
 def demo_basic_auth_credentials(
-    credentials: Annotated[HTTPBasicCredentials, Depends(security)]
+    credentials: Annotated[HTTPBasicCredentials, Depends(security)],
 ):
     return {
-        "message": "Hi",
+        "message": "Hi!",
         "username": credentials.username,
         "password": credentials.password,
     }
@@ -24,127 +24,118 @@ def demo_basic_auth_credentials(
 
 usernames_to_passwords = {
     "admin": "admin",
-    "nick": "password",
+    "john": "password",
 }
+
 
 static_auth_token_to_username = {
-    "de884cf9e1188b64c6e3d89667d4bd78": "admin",
-    "cee6e2f0aaf817ffeb21f0edc8b4afe7": "john",
+    "a0787852e766b02e87f6dd15e4c3d1f1": "admin",
+    "a14f178e75dee69fa66ff3fad9db0daa": "john",
 }
-
-
-def get_username_by_static_token(
-    static_token: str = Header(alias="static-auth-token"),
-) -> str:
-    # Вариант 1
-    if username := static_auth_token_to_username.get(static_token):
-        return username
-    raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid static auth token",
-    )
-
-    # Вариант 2
-    # if static_token not in static_auth_token_to_username:
-    #     raise HTTPException(
-    #         status_code=status.HTTP_401_UNAUTHORIZED,
-    #         detail="Invalid static token",
-    #     )
-    #
-    # return static_auth_token_to_username[static_token]
 
 
 def get_auth_user_username(
-    credentials: Annotated[HTTPBasicCredentials, Depends(security)]
-):
-    unauth_exp = HTTPException(
+    credentials: Annotated[HTTPBasicCredentials, Depends(security)],
+) -> str:
+    unauthed_exc = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Incorrect username or password",
+        detail="Invalid username or password",
         headers={"WWW-Authenticate": "Basic"},
     )
-
     correct_password = usernames_to_passwords.get(credentials.username)
-    if not correct_password:
-        raise unauth_exp
+    if correct_password is None:
+        raise unauthed_exc
 
-    if credentials.username not in usernames_to_passwords:
-        raise unauth_exp
-
+    # secrets
     if not secrets.compare_digest(
         credentials.password.encode("utf-8"),
         correct_password.encode("utf-8"),
     ):
-        raise unauth_exp
+        raise unauthed_exc
 
     return credentials.username
 
 
-@router.get("/basic-auth-username")
+def get_username_by_static_auth_token(
+    static_token: str = Header(alias="x-auth-token"),
+) -> str:
+    if username := static_auth_token_to_username.get(static_token):
+        return username
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="token invalid",
+    )
+
+
+@router.get("/basic-auth-username/")
 def demo_basic_auth_username(
     auth_username: str = Depends(get_auth_user_username),
 ):
     return {
-        "message": "Hi",
+        "message": f"Hi, {auth_username}!",
         "username": auth_username,
     }
 
 
-@router.get("/some-https-header-auth")
-def demo_auth_some_http_header(username: str = Depends(get_username_by_static_token)):
+@router.get("/some-http-header-auth/")
+def demo_auth_some_http_header(
+    username: str = Depends(get_username_by_static_auth_token),
+):
     return {
         "message": f"Hi, {username}!",
         "username": username,
     }
 
 
-COOKIES: dict[str, dict[str, any]] = {}
-
+COOKIES: dict[str, dict[str, Any]] = {}
 COOKIE_SESSION_ID_KEY = "web-app-session-id"
 
 
-def generate_session_id():
+def generate_session_id() -> str:
     return uuid.uuid4().hex
 
 
-def get_session_data(session_id: str = Cookie(alias=COOKIE_SESSION_ID_KEY)):
+def get_session_data(
+    session_id: str = Cookie(alias=COOKIE_SESSION_ID_KEY),
+) -> dict:
     if session_id not in COOKIES:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Not authenticated",
+            detail="not authenticated",
         )
 
     return COOKIES[session_id]
 
 
-@router.post("/login/cookie")
-def demo_auth_login_cookie(
+@router.post("/login-cookie/")
+def demo_auth_login_set_cookie(
     response: Response,
-    auth_username: str = Depends(get_auth_user_username),
-    # username: str = Depends(get_username_by_static_token)
+    # auth_username: str = Depends(get_auth_user_username),
+    username: str = Depends(get_username_by_static_auth_token),
 ):
-
     session_id = generate_session_id()
     COOKIES[session_id] = {
-        "username": auth_username,
+        "username": username,
         "login_at": int(time()),
     }
     response.set_cookie(COOKIE_SESSION_ID_KEY, session_id)
-    return {"message": "ok"}
+    return {"result": "ok"}
 
 
-@router.get("/check-cookie")
+@router.get("/check-cookie/")
 def demo_auth_check_cookie(
     user_session_data: dict = Depends(get_session_data),
 ):
     username = user_session_data["username"]
     return {
-        "message": f"Hi, {username}!",
-        "username": username,
+        "message": f"Hello, {username}!",
+        **user_session_data,
     }
 
 
-@router.get("/logout-cookie")
-def demo_auth_check_cookie(
+@router.get("/logout-cookie/")
+def demo_auth_logout_cookie(
     response: Response,
     session_id: str = Cookie(alias=COOKIE_SESSION_ID_KEY),
     user_session_data: dict = Depends(get_session_data),
--- users/schemas.py
+++ users/schemas.py
@@ -1,8 +1,16 @@
 from annotated_types import MaxLen, MinLen
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, ConfigDict
 from typing_extensions import Annotated
 
 
 class CreateUser(BaseModel):
     username: Annotated[str, MaxLen(30), MinLen(6)]
     email: EmailStr
+
+class UserSchema(BaseModel):
+    model_config = ConfigDict(strict=True)
+
+    username: str
+    password: bytes
+    active: bool = True
+    email: EmailStr | None = None
